b3a2c45ba44a7ebab3b1af4b4b850c168f2d919a
diaabadr <diaabadr82@gmail.com>
2023-03-26 22:49:27 +0200

    init

---
+++ src/extension.ts
@@ -0,0 +1,15 @@
+import * as vscode from "vscode";
+
+let prayingInterval: ReturnType<typeof setInterval>;
+const prayingIntervalTime = 1000 * 20;
+
+export function activate(context: vscode.ExtensionContext) {
+  prayingInterval = setInterval(() => {
+    vscode.window.showInformationMessage("صلي على النبي محمد ﷺ");
+  }, prayingIntervalTime);
+}
+
+// This method is called when your extension is deactivated
+export function deactivate() {
+  clearInterval(prayingInterval);
+}
