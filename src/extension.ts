19259a45832722d5b4005785b13d8bc598f1eb22
diaabadr <diaabadr82@gmail.com>
2023-04-10 16:45:00 +0200

    fixing intervals overrideing

--- src/extension.ts
+++ src/extension.ts
@@ -2,14 +2,19 @@ import * as vscode from "vscode";
 
 let prayingInterval: ReturnType<typeof setInterval>;
 const prayingIntervalTime = 1000 * 60 * 20;
-
+let intervalRunning = false;
 export function activate(context: vscode.ExtensionContext) {
+  if (intervalRunning) {
+    return;
+  }
   prayingInterval = setInterval(() => {
     vscode.window.showInformationMessage("صلي على النبي محمد ﷺ");
+    intervalRunning = true;
   }, prayingIntervalTime);
 }
 
 // This method is called when your extension is deactivated
 export function deactivate() {
   clearInterval(prayingInterval);
+  intervalRunning = false;
 }
